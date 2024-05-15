838c97b8ead4923ab983b1fb8bba2d996e04979f
Aditya Krishna <krish.adit@gmail.com>
2024-05-14 21:45:05 -0400

    Added Comments and Minor Fixes

--- tremor_detect/src/main.cpp
+++ tremor_detect/src/main.cpp
@@ -2,24 +2,14 @@
 #include <arm_math.h>
 #include "drivers/LCD_DISCO_F429ZI.h"
 
-// =================================================
-// * Recitation 5: SPI and Gyroscope *
-// =================================================
-
-// TODOs:
-// [1] Get started with an SPI object instance and connect to the Gyroscope!
-// [2] Read the XYZ axis from the Gyroscope and Visualize on the Teleplot. 
-// [3] Fetching Data from the sensor via Polling vs Interrupt ?
-
-// Define control register addresses and their configurations
 #define CTRL_REG1 0x20
 #define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
 #define CTRL_REG4 0x23
 #define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
 #define SPI_FLAG 1
 #define OUT_X_L 0x28
-#define lower_threshold 50
-#define upper_threshold 2000
+#define LOWER_THRESHOLD 100
+#define UPPER_THRESHOLD 2000
 
 EventFlags flags;
 LCD_DISCO_F429ZI lcd;
@@ -30,39 +20,6 @@ void spi_cb(int event)
 }
 
 #define SCALING_FACTOR (17.5f * 0.0174532925199432957692236907684886f / 1000.0f)
-
-bool binary_classifier(float* esd)
-{
-    float energy_value = esd[1] + esd[2] + esd[3];
-
-    // printf("Energy Value =%f", energy_value);
-    if (energy_value > lower_threshold && energy_value < upper_threshold)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-
-}
-
-// void  printLCD(float* esd)
-// {
-    // if (binary_classifier(&esd[0])) {
-    //     printf("Tremor detected\n");
-    //     lcd.Clear(LCD_COLOR_RED);
-    //     lcd.SetBackColor(LCD_COLOR_RED);
-    //     lcd.SetTextColor(LCD_COLOR_WHITE);
-    //     lcd.DisplayStringAtLine(5, (uint8_t *)"Tremor detected");
-    // } else {
-    //     printf("No tremor detected\n");
-    //     lcd.Clear(LCD_COLOR_GREEN);
-    //     lcd.SetBackColor(LCD_COLOR_GREEN);
-    //     lcd.SetTextColor(LCD_COLOR_BLACK);
-    //     lcd.DisplayStringAtLine(5, (uint8_t *)"Monitoring...");
-    // }
-// }
 
 void energy_spectral_density(int n_samples, float32_t *sample_buffer, float32_t *esd) {
     // computes discrete energy (ESD) spectral density from FFT
@@ -87,9 +44,7 @@ void energy_spectral_density(int n_samples, float32_t *sample_buffer, float32_t 
 
     arm_rfft_fast_f32(&handler, sample_buffer, output_ptr, 0);
 
-    // printf("fft: %3.2f\n", output_buffer[0]);
-
-    // apparently the output buffer of ARM FFT is like this:
+    // The output buffer of ARM FFT is like this:
     // i0_real, i0_complex, i1_real, i1_complex, we we square
     // and add the corresponding real and complex parts
 
@@ -97,6 +52,8 @@ void energy_spectral_density(int n_samples, float32_t *sample_buffer, float32_t 
     float a;  // real part of FFT bin
     float b;  // complex part of FFT bin
     float value;
+    char disp_text_freq[16];
+    char disp_text_int[16];
     // printf(">esd:0|g,clr\n");
     for (i = 0; i < 127; i+=2) {
         a = output_buffer[i];  // real
@@ -105,12 +62,20 @@ void energy_spectral_density(int n_samples, float32_t *sample_buffer, float32_t 
         // printf(">esd: %3.2f|g \n", value);
         esd[i / 2] = value;  // ESD for this bin
     }
-    // printLCD(&esd[0]);
-    // flag = binary_classifier(&esd[0]);
-    energy_value = esd[1] + esd[2] + esd[3];
 
+    // the sum of energy at 2-4hz and 4-6hz to get the overall range of 2-6hz
+    energy_value = esd[1] + esd[2];
+
+    if (esd[1] > esd[2]) {
+        sprintf(disp_text_freq, "Frequency: 2-4Hz ");
+        sprintf(disp_text_int, "Intensity: %.0f", esd[1]);
+    } else {
+        sprintf(disp_text_freq, "Frequency: 4-6Hz ");
+        sprintf(disp_text_int, "Intensity: %.0f", esd[2]);
+    }
     printf("Energy Value =%f", energy_value);
-    if (energy_value > lower_threshold && energy_value < upper_threshold)
+    //binary classifier that makes sure the total energy value over the range of 2-6hz corresponds to a tremor
+    if (energy_value > LOWER_THRESHOLD && energy_value < UPPER_THRESHOLD)
     {
         flag = true;
     }
@@ -118,12 +83,15 @@ void energy_spectral_density(int n_samples, float32_t *sample_buffer, float32_t 
     {
         flag = false;
     }
+    //set the LCD based on whether a tremor was detected or not
     if (flag == 1) {
         printf("Tremor detected\n");
         lcd.Clear(LCD_COLOR_RED);
         lcd.SetBackColor(LCD_COLOR_RED);
         lcd.SetTextColor(LCD_COLOR_WHITE);
         lcd.DisplayStringAt(10,LINE(8),(uint8_t *)"Tremor detected",CENTER_MODE);
+        lcd.DisplayStringAt(10,LINE(10),(uint8_t *)disp_text_freq,CENTER_MODE);
+        lcd.DisplayStringAt(10,LINE(12),(uint8_t *)disp_text_int,CENTER_MODE);
     } else {
         printf("No tremor detected\n");
         lcd.Clear(LCD_COLOR_GREEN);
@@ -134,7 +102,6 @@ void energy_spectral_density(int n_samples, float32_t *sample_buffer, float32_t 
 }
 
 void collect_data(int interval, int n_samples, float32_t *sample_buffer) {
-    // TODO: See if spi object and flags can be initialized just once in main() and then passed into collect_data
     // Initialize the SPI object with specific pins.
     SPI spi(PF_9, PF_8, PF_7, PC_1, use_gpio_ssel);
 
@@ -178,18 +145,12 @@ void collect_data(int interval, int n_samples, float32_t *sample_buffer) {
         // printf("RAW \t\tgx: %d \t gy: %d \t gz: %d \t\n", raw_gx, raw_gy, raw_gz);
 
         // Convert raw data to actual values using a scaling factor
-        gx = ((float) raw_gx) * SCALING_FACTOR;
+        // The y-axis corresponds to the roll axis, the axis that corresponds to tremor movement
         gy = ((float) raw_gy) * SCALING_FACTOR;
-        gz = ((float) raw_gz) * SCALING_FACTOR;
 
         sample_buffer[sample_ix] = gy;
 
-        // Print the actual values
-        // printf("Actual -> \t\tgx: %4.5f \t gy: %4.5f \t gz: %4.5f \t\n", gx, gy, gz);
-
-        // printf(">x_axis: %5.2f|g \n", gx);
         // printf(">y_axis: %5.2f|g \n", gy);
-        // printf(">z_axis: %5.2f|g \n", gz);
 
         thread_sleep_for(interval);
     }
@@ -198,17 +159,18 @@ void collect_data(int interval, int n_samples, float32_t *sample_buffer) {
 
 int main()
 {
-    // collect data every 4ms
-    // sampling rate = 1s / interval = 250Hz
-    int interval = 4;
+    // collect data every 8ms
+    // sampling rate = 1s / interval = 125Hz
+    int interval = 8;
 
-    // collect 128 samples for a total of 512ms of data
+    // collect 128 samples for a total of 1024ms of data
     int n_samples = 128;
 
     // a buffer to store the samples, array of size n_samples
     float32_t sample_buffer[128];
     float32_t esd[128];
 
+    // Initializing the LCD
     lcd.Clear(LCD_COLOR_GREEN);
     lcd.SetBackColor(LCD_COLOR_GREEN);
     lcd.SetTextColor(LCD_COLOR_BLACK);
